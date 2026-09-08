48817f554bb4762d8cef3cae37d6bea83d878b41
Mihir-Paul <mihirpgm@gmail.com>
2026-09-08 18:27:48 +0530

    Add additional examples for 2D and 3D NumPy arrays in array.py

--- numpy module/array.py
+++ numpy module/array.py
@@ -5,3 +5,12 @@ print(array0d.ndim)
 arr1d = np.array(['A','B','C'])
 print(arr1d.ndim)
 
+arr2d = np.array([['A','B','C'],
+                 ['D','E','F'],
+                 ['G','H','I']])
+print(arr2d.ndim)
+
+arr3d = np.array([[['A','B','C'],['D','E','F'],['G','H','I']],
+                 [['J','K','L'],['M','N','O'],['P','Q','R']],
+                 [['S','T','U'],['V','W','X'],['Y','Z',' ']]])
+print(arr3d.ndim)
