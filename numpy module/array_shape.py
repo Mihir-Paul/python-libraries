8e3c39b4effedcad058ef1b9c2ab50f882c5958a
Mihir-Paul <mihirpgm@gmail.com>
2026-09-08 19:01:30 +0530

    Add array_shape.py to demonstrate the shape of 2D and 3D NumPy arrays

---
+++ numpy module/array_shape.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr3d = np.array([[['A','B','C'],['D','E','F'],['G','H','I']],
+                 [['J','K','L'],['M','N','O'],['P','Q','R']],
+                 [['S','T','U'],['V','W','X'],['Y','Z',' ']]])
+print(arr3d.shape)
+
+arr2d = np.array([['A','B','C'],
+                 ['D','E','F'],])
+print(arr2d.shape)
