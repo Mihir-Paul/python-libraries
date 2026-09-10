4c822fc937e9e824837c428da013c2b0d66eb723
Mihir-Paul <mihirpgm@gmail.com>
2026-09-10 23:39:17 +0530

    Add broadcasting example in broadcasting.py to demonstrate array multiplication with NumPy

---
+++ numpy module/broadcasting.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr1 = np.array([[1,2,3,4],
+                 [2,4,6,8],
+                 [4,8,12,16],
+                 [8,16,24,32]])
+arr2 = np.array([[5,6,7,8]])
+
+print(arr1.shape)
+print (arr2.shape)
+
+print(arr1*arr2)
